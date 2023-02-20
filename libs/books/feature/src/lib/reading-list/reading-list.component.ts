d9dec2fd4ac629a84fe5a63802e13b73bf1deb2f
RGiri2 <Rajat.Giri2@t-mobile.com>
2023-02-20 12:28:27 -0800

    Task 3: Undo add and remove from reading list

--- libs/books/feature/src/lib/reading-list/reading-list.component.ts
+++ libs/books/feature/src/lib/reading-list/reading-list.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { getReadingList, removeFromReadingList } from '@tmo/books/data-access';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import {
+  getReadingList,
+  removeFromReadingList,
+  undoRemoveFromReadingList,
+} from '@tmo/books/data-access';
+import { ReadingListItem } from '@tmo/shared/models';
 
 @Component({
   selector: 'tmo-reading-list',
@@ -10,9 +16,20 @@ import { getReadingList, removeFromReadingList } from '@tmo/books/data-access';
 export class ReadingListComponent {
   readingList$ = this.store.select(getReadingList);
 
-  constructor(private readonly store: Store) {}
+  constructor(private readonly store: Store, private _snackBar: MatSnackBar) {}
 
-  removeFromReadingList(item) {
-    this.store.dispatch(removeFromReadingList({ item }));
+  removeFromReadingList(item: ReadingListItem) {
+    const _snackBarRef = this._snackBar.open(
+        'Removed from the reading list',
+        'Undo',
+        {
+          duration: 4000,
+        }
+    );
+    this.store.dispatch(removeFromReadingList({item}));
+
+    _snackBarRef.onAction().subscribe(() => {
+      this.store.dispatch(undoRemoveFromReadingList({item}));
+    });
   }
 }
